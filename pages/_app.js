0b0769760dab6b85f321169f8da4627e35684330
Vinod Patel <v_p88@hotmail.com>
2022-01-30 11:02:28 +0000

    added bootstrapJS import via a useEffect hook on initial render

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import "../styles/globals.css";
 import Layout from "../Components/Layout";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    import("../node_modules/bootstrap/dist/js/bootstrap.min.js");
+  }, []);
   return (
     <Layout>
       <Component {...pageProps} />{" "}

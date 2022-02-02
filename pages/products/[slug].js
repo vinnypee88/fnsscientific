dfd65a5290e0ad595fbe9f4f196a2fed18cc0953
Vinod Patel <v_p88@hotmail.com>
2022-02-02 15:50:15 +0000

    Responsive layout edited in product slug page

--- pages/products/[slug].js
+++ pages/products/[slug].js
@@ -38,13 +38,13 @@ export async function getStaticProps({ params }) {
 const ProductDetails = ({ product }) => {
   const { productTitle, productImages, productDescription } = product.fields;
   return (
-    <div className="mt-5">
+    <div className="mt-5 m-1">
       <div className="m-5 pt-5">
         <h2 className="text-center mt-5 text-blue-800">{productTitle}</h2>
       </div>
 
-      <div className="m-5 container row">
-        <div className="col-6">
+      <div className="m-0 m-lg-5 p-0 row justify-content-center">
+        <div className="col-10 col-md-6 p-0 p-md-3">
           {" "}
           <Image
             src={"https:" + productImages[0].fields.file.url}
@@ -52,7 +52,7 @@ const ProductDetails = ({ product }) => {
             height={productImages[0].fields.file.details.image.height}
           />
         </div>
-        <div className="col-6">
+        <div className="col-10 col-md-6 p-0 p-md-3">
           {documentToReactComponents(productDescription)}
         </div>
       </div>

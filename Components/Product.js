b10790b2e03540f3516f144f2e1b3066a9bed399
Vinod Patel <v_p88@hotmail.com>
2022-02-03 08:26:02 +0000

    Added links to the images in the product page for easier navigation to the product specific page

--- Components/Product.js
+++ Components/Product.js
@@ -6,15 +6,19 @@ const Product = ({ product }) => {
   return (
     <div className="m-3 col-11 col-sm-5 col-lg-3">
       <div className="card">
-        <Image
-          src={"https:" + productThumbnail.fields.file.url}
-          width={1300}
-          height={900}
-          className="card-img-top"
-        />
+        <Link href={"/products/" + slug}>
+          <a>
+            <Image
+              src={"https:" + productThumbnail.fields.file.url}
+              width={1300}
+              height={900}
+              className="card-img-top"
+            />
+          </a>
+        </Link>
         <div className="card-body">
           <Link href={"/products/" + slug}>
-            <a className="card-title">{productTitle}</a>
+            <a className="card-title fw-bold text-blue-800">{productTitle}</a>
           </Link>
           {/* <p className="card-text">
             Some quick example text to build on the card title and make up the

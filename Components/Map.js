b44fd9877a76354c3c27285fc513740cd2123907
Vinod Patel <v_p88@hotmail.com>
2022-02-08 09:38:42 +0000

    Changed tile layer to mapbox, added coordinates

--- Components/Map.js
+++ Components/Map.js
@@ -6,17 +6,17 @@ import "leaflet-defaulticon-compatibility";
 const Map = () => {
   return (
     <MapContainer
-      center={[40.8054, -74.0241]}
+      center={[-1.26286, 36.8044]}
       zoom={14}
       scrollWheelZoom={false}
       style={{ height: "100%", width: "100%" }}
     >
       <TileLayer
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        url={`https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.NEXT_PUBLIC_MAP_BOX_TOKEN}`}
+        attribution='Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>'
       />
-      <Marker position={[40.8054, -74.0241]} draggable={true} animate={true}>
-        <Popup>Hey ! you found me</Popup>
+      <Marker position={[-1.26286, 36.8044]} draggable={true} animate={true}>
+        <Popup>FNScientific!</Popup>
       </Marker>
     </MapContainer>
   );

58fda0cfc2229cbe32b15fae32f3093007267b9e
Vinod Patel <v_p88@hotmail.com>
2022-02-13 14:58:20 +0000

    Added some animation to the numbers in the client stats component

--- Components/ClientStat.js
+++ Components/ClientStat.js
@@ -1,8 +1,28 @@
-const ClientStat = ({ num, text }) => {
+import { gsap } from "gsap/dist/gsap.js";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+import { useEffect } from "react";
+
+const ClientStat = ({ num, text, uniqueID }) => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(`#${uniqueID}`, {
+      scrollTrigger: {
+        trigger: `#${uniqueID}`,
+        toggleActions: "restart none none none",
+      },
+      innerText: num,
+      snap: "innerText",
+      duration: 1.5,
+    });
+  }, []);
+
   return (
     <div className="container col-xs-10 col-sm-8 col-md-3 m-2 text-center rounded-3 p-0">
       <div className="p-3">
-        <h3 className="py-1 display-1 fw-bold text-orange">{num}</h3>
+        <h3
+          id={uniqueID}
+          className="py-1 display-1 fw-bold text-orange number"
+        ></h3>
         <p className="text-orange fw-bold">{text}</p>
       </div>
     </div>
--- Components/ClientStats.js
+++ Components/ClientStats.js
@@ -11,9 +11,9 @@ const ClientStats = () => {
       id="about-client-stats"
       className="row justify-content-center mx-3 py-2 py-lg-5"
     >
-      <ClientStat num="800" text="Customers we serve" />
-      <ClientStat num="9" text="Operating Countries" />
-      <ClientStat num="18" text="Supply Partners" />
+      <ClientStat uniqueID="stat-1" num="800" text="Customers we serve" />
+      <ClientStat uniqueID="stat-2" num="9" text="Operating Countries" />
+      <ClientStat uniqueID="stat-3" num="18" text="Supply Partners" />
     </div>
   );
 };
